f7614199630d54c71b112c75594645a1d5b7ebce
RakeMeUp <bencehetes@gmail.com>
2022-08-31 00:53:29 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useSelector } from 'react-redux'
 import './App.scss'
+import { RootState } from './redux/store'
+
+import { bankrupt, decrease, increase } from './redux/slices/counterSlice'
+import { useDispatch } from 'react-redux'
+
 function App() {
+
+  const count = useSelector((state: RootState)=> state.count)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
-      <h1>0</h1>
-      <button>Deposit</button>
-      <button>Withdraw</button>
-      <button>Bankrupt</button>
+      <h1>{count}</h1>
+      <button onClick={()=> dispatch(increase(100))}>Deposit</button>
+      <button onClick={()=> dispatch(decrease(100))}>Withdraw</button>
+      <button onClick={()=> dispatch(bankrupt())}>Bankrupt</button>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 import {Provider} from 'react-redux'
-import {store} from "./state/store"
+import {store} from "./redux/store"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
